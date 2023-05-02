afb414f8b3616c7302e1f2e8ae04e34d9e9e704f
33Luvy <33Luvy@gmail.com>
2023-05-02 16:18:20 +0100

    Added row and cell variable, rework of prev function

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const grid = document.getElementsByClassName('grid')
+let rows = document.getElementsByClassName('gridRow')
+let cells = document.getElementsByClassName('gridCell')
 
-function createGrid(numCells) {
-    for (let x = 0; x < numCells; x++) {
-        let cells = document.createElement('div');
-        grid.appendChild(cells)
+function createRows(numRows) {
+    for (let x = 0; x < numRows; x++) {
+        let row = document.createElement('div');
+        grid.appendChild(row).className = 'gridRow';
     }
 }
+
