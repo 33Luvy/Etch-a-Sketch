baf7522291e4d9354f426de4d756c84ca0440538
33Luvy <33Luvy@gmail.com>
2023-05-03 15:11:54 +0100

    Reworked createGrid function as it didn't work correctly

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const grid = document.getElementsByClassName('grid')
-let rows = document.getElementsByClassName('gridRow')
-let cells = document.getElementsByClassName('gridCell')
+const grid = document.getElementById('grid');
 
-function createRows(numRows) {
-    for (let x = 0; x < numRows; x++) {
-        let row = document.createElement('div');
-        grid.appendChild(row).className = 'gridRow';
+// Function for creating the grid
+function createGrid(gridNum) {
+    // For loop creating the number of cells
+    for (x = 0; x < gridNum; x++) {
+        let rows = document.createElement('div')
+        grid.appendChild(rows).className = 'gridRow';
+        for (c = 0; c < gridNum; c++) {
+            let cells = document.createElement('div')
+            rows.appendChild(cells).className = 'gridCell';
+        }
     }
 }
 
+createGrid(16)
